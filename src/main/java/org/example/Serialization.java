c2a16d9e5ad04f93b975d0e168d4782062c9ca3b
unaPalabra <122109082+unaPalabra@users.noreply.github.com>
2023-11-12 18:11:08 +0700

    first

--- src/main/java/org/example/Serialization.java
+++ src/main/java/org/example/Serialization.java
@@ -3,21 +3,32 @@ package org.example;
 import org.example.dto.StudentData;
 
 import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Serialization {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         StudentData studentData = new StudentData();
-        studentData.setFirstName("Egor");
+        studentData.setFirstName("Егор");
         studentData.setLastName("Летов");
         studentData.setGrade(4);
 
+//        сериализация (запись данных в файл на рабочем столе)
         try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(Files.newOutputStream(Paths.get("C:\\Users\\Don_Aleut\\Desktop\\java_test\\egor.data")))) {
             objectOutputStream.writeObject(studentData);
             objectOutputStream.flush();
         }
+
+//        десериализация (восстановление данных из файла)
+        ObjectInputStream objectInputStream = new ObjectInputStream(Files.newInputStream(Paths.get("C:\\Users\\Don_Aleut\\Desktop\\java_test\\egor.data")));
+        StudentData o = (StudentData)objectInputStream.readObject();
+        System.out.println(o.getFirstName());
+        System.out.println(o.getLastName());
+        System.out.println(o.getGrade());
+
 
 
     }
--- src/main/java/org/example/dto/StudentData.java
+++ src/main/java/org/example/dto/StudentData.java
@@ -1,21 +1,17 @@
 package org.example.dto;
 
 import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JacksonAnnotation;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class StudentData  implements Serializable{
-
-        private Long id;
+    @JsonProperty("firsName")
         private String firstName;
+    @JsonProperty("lastName")
         private String lastName;
+    @JsonProperty("grade")
         private int grade;
 
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
 
         public String getFirstName() {
             return firstName;
